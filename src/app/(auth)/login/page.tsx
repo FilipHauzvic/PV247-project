4088d509a531ab88c47e1c0d4d18013b0244cb8b
Martin Stalmach <martin.stalmach21@gmail.com>
2025-11-23 15:46:21 +0100

    fix: fix auth pages format

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const LoginPage = () => {
 	return (
 		<form className="flex flex-col items-start justify-start gap-4">
-			<h1 className="text-3xl p-2">Login</h1>
+			<h1 className="text-3xl">Login</h1>
 			<InputLabel htmlFor="username">Username</InputLabel>
 				<Input
 					id="username"
@@ -22,7 +22,7 @@ const LoginPage = () => {
 				</Button>
 			</div>
 			
-			<Link href="/login" className="mt-4 text-blue-500 hover:underline block">
+			<Link href="/register" className="text-blue-500 hover:underline block">
 				Don't have an account? Register here.
 			</Link>
 		</form>
