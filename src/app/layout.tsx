e5e98dd5724a76a4ce9455b1c4d1309b97629134
Martin Stalmach <martin.stalmach21@gmail.com>
2025-11-30 14:44:13 +0100

    feat: prepare root layout metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,29 +5,40 @@ import { Providers } from "./providers";
 import { Roboto } from "next/font/google";
 
 const roboto = Roboto({
-  weight: ["300", "400", "500", "700"],
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-roboto",
+	weight: ["300", "400", "500", "700"],
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-roboto",
 });
 
 export const metadata: Metadata = {
-  title: "Movie Emoji Guesser",
+	metadataBase: new URL("http://localhost:3000"),
+	title: { default: "Movie Emoji Guesser", template: "%s | Movie Emoji Guesser" },
+	description: "A simple game for guessing movies based on emojis.",
+	alternates: {
+		canonical: "http://localhost:3000",
+	},
+	openGraph: {
+		title: "Movie Emoji Guesser",
+		description: "A simple game for guessing movies based on emojis.",
+		url: "http://localhost:3000",
+		siteName: "Movie Emoji Guesser",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={roboto.variable} style={{ margin: 0 }}>
-        <Providers>
-          <GuesserNavBar />
-          {children}
-        </Providers>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={roboto.variable} style={{ margin: 0 }}>
+			<Providers>
+				<GuesserNavBar />
+				{children}
+			</Providers>
+			</body>
+		</html>
+	);
 }
