5deb19692d4231fa4a8c1521fa75a48531d889e2
Martin Stalmach <martin.stalmach21@gmail.com>
2025-12-08 18:55:34 +0100

    fix: remove bad text from rebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,7 +61,7 @@ const Page = () => {
           		{error}
         	</div> : listLoading ? (
 				<div className="flex justify-center mt-50">
-					<CircularProgress />lement home page with link to sign in page)
+					<CircularProgress />
 				</div>
 			) : (
 				<div className="flex justify-center">
